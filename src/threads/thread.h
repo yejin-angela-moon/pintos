11e43619d369e785f960c366deeac0736ebeb68d
le522 <le522@ic.ac.uk>
2023-10-15 12:44:03 +0100

    changed thread.h to work with the prev changes to lock

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,7 +88,8 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
-    int donated_priority;               /* Priority donated by other thread. */
+    // int donated_priority;               /* Priority donated by other thread. */
+    int original_priority;
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
