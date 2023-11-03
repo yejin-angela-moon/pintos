e0990f3a9432a9e727ef05b2fe463a5f986e5203
ym1721 <ym1721@ic.ac.uk>
2023-11-03 13:27:12 +0000

    feat: argument passing by setting up+pushing onto stack

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -3,6 +3,9 @@
 
 #include "threads/thread.h"
 
+#define MAX_ARGS 4
+#define WORD_ALIGN_MASK 00000011
+
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
