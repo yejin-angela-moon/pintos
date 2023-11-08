18618d4d835e7298b4302a9379b22c5b88dfa1d3
Kevin Halm Alvarez <kmh22@ray16.doc.ic.ac.uk>
2023-11-08 16:29:08 +0000

    merged argument passing

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -31,62 +31,62 @@ syscall_handler(struct intr_frame *f) {
             break;
         }
         case SYS_EXEC: { /**/
-            const char *cmd_line = *((const char **)(f->esp + 4));
+            const char *cmd_line = *((const char **) (f->esp + 4));
             f->eax = (uint32_t) exec(cmd_line);
             break;
         }
         case SYS_WAIT: {
-            pid_t pid = *((pid_t *)(f->esp +4));
+            pid_t pid = *((pid_t * )(f->esp + 4));
             f->eax = (uint32_t) wait(pid);
             break;
         }
         case SYS_CREATE: { /* Create a file. */
-            const char *file = *((const char **)(f->esp + 4));
-            unsigned initial_size = *((unsigned *)(f->esp + 8));
+            const char *file = *((const char **) (f->esp + 4));
+            unsigned initial_size = *((unsigned *) (f->esp + 8));
             f->eax = (uint32_t) create(file, initial_size);
             break;
-        }            
+        }
         case SYS_REMOVE: { /* Delete a file. */
-            const char *file = *((const char **)(f->esp + 4));
+            const char *file = *((const char **) (f->esp + 4));
             f->eax = (uint32_t) remove(file);
             break;
-        }               
+        }
         case SYS_OPEN: {  /* Open a file. */
-            const char *file = *((const char **)(f->esp + 4));
+            const char *file = *((const char **) (f->esp + 4));
             f->eax = (uint32_t) open(file);
             break;
-        }                  
+        }
         case SYS_FILESIZE: { /* Obtain a file's size. */
             int fd = *((int *) (f->esp + 4));
             f->eax = (uint32_t) filesize(fd);
             break;
-        }             
+        }
         case SYS_READ: {  /* Read from a file. */
             int fd = *((int *) (f->esp + 4));
-            void *buffer = *((void **)(f->esp + 8));
-            unsigned size = *((unsigned *)(f->esp + 12));
+            void *buffer = *((void **) (f->esp + 8));
+            unsigned size = *((unsigned *) (f->esp + 12));
             f->eax = (uint32_t) read(fd, buffer, size);
             break;
-        }                  
+        }
         case SYS_WRITE: { /* Write to a file. */
             int fd = *((int *) (f->esp + 4));
-            const void *buffer = *((const void **)(f->esp + 8));
-            unsigned size = *((unsigned *)(f->esp + 12));
+            const void *buffer = *((const void **) (f->esp + 8));
+            unsigned size = *((unsigned *) (f->esp + 12));
             f->eax = (uint32_t) write(fd, buffer, size);
             break;
-        }               
+        }
         case SYS_SEEK: { /* Change position in a file. */
             int fd = *((int *) (f->esp + 4));
-            unsigned position = *((unsigned *)(f->esp + 8));
+            unsigned position = *((unsigned *) (f->esp + 8));
             seek(fd, position);
             break;
-        }                   
+        }
         case SYS_TELL: { /* Report current position in a file. */
             int fd = *((int *) (f->esp + 4));
             f->eax = (uint32_t) tell(fd);
             break;
-        }                   
-        case SYS_CLOSE: { 
+        }
+        case SYS_CLOSE: {
             int fd = *((int *) (f->esp + 4));
             close(fd);
             break;
@@ -112,8 +112,8 @@ exit(int status) {
     thread_exit();
 }
 
-pid_t 
-exec(const char *cmd_line){
+pid_t
+exec(const char *cmd_line) {
     //TODO
 }
 
@@ -123,65 +123,65 @@ wait(pid_t pid) {
     return process_wait(pid);
 }
 
-bool 
+bool
 create(const char *file, unsigned initial_size) {
     return filesys_create(file, initial_size);
 }
 
-bool 
+bool
 remove(const char *file) {
     return filesys_remove(file);
 }
 
-int 
+int
 open(const char *file) {
     struct file *f = filesys_open(file);
     if (f == NULL) {
         // File could not be opened
-        return -1; 
+        return -1;
     } else {
         // Add the file to the process's open file list and return the file descriptor
         return process_add_file(f);
     }
 }
 
-int 
+int
 filesize(int fd) {
     //TODO
 }
 
-int 
+int
 read(int fd, void *buffer, unsigned size) {
     //TODO
 }
 
-int 
+int
 write(int fd, const void *buffer, unsigned size) {
     if (fd == 1) {  // writes to console
         for (int j; j < size; j += 200)  // max 200B at a time
-            putbuf(buffer + j, min(200+j, size);
+            putbuf(buffer + j, min(200 + j, size);
         return size;
     }
     int i;
     for (i = 0; i < size; i++) {
-        if (!put_user(fd+i, buffer+i))
+        if (!put_user(fd + i, buffer + i))
             break;
     }
     return i;
 
 }
 
-void 
+void
 seek(int fd, unsigned position) {
     //TODO
 }
 
-unsigned 
+unsigned
 tell(int fd) {
     //TODO
 }
 
-void 
+void
 close(int fd) {
     //TODO
 }
