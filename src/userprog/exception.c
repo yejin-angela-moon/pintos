099e20ede9aedff22f9e8c76733d37e0e7a1808a
Kevin Halm Alvarez <kmh22@vertex16.doc.ic.ac.uk>
2023-12-07 12:40:32 +0000

    Made Files the only shareable spte type

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -232,9 +232,12 @@ page_fault (struct intr_frame *f)
         lock_release(&page_sharing_lock);
 
         uint8_t *kpage = NULL;
-        if (found_shared_page != NULL && !spte->writable) {
+        if (found_shared_page != NULL && !spte->writable && spte->type == File) {
             // If page is shared and read-only, use existing kpage.
-            kpage = found_shared_page->kpage;
+            kpage = pagedir_get_page(found_shared_page->pagedir, spte->user_vaddr);
+            pagedir_set_page(cur->pagedir, spte->user_vaddr, kpage, false);
+            found_shared_page->shared_count += 1;
+
         } else {
             // Allocate a new frame if page not shared or writable.
             kpage = pagedir_get_page(cur->pagedir, spte->user_vaddr);
@@ -247,12 +250,11 @@ page_fault (struct intr_frame *f)
             load_page(spte, kpage);
 
             // If page is read-only, consider sharing it.
-            if (!spte->writable) {
+            if (!spte->writable && spte->type == File) {
                 // Here you can either use the share_page function or write the logic directly.
                 // Ensure to update the shared_page struct with kpage and pagedir.
                 struct shared_page new_shared_page;
                 new_shared_page.spte = spte;
-                new_shared_page.kpage = kpage;
                 new_shared_page.pagedir = cur->pagedir;
                 new_shared_page.shared_count = 1;
 
